d281f7befd8398ad7483afe9eb5f91e4c825f55e
Paulprogo <136633326+Paulprogo@users.noreply.github.com>
2023-12-13 11:45:53 -0600

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -17,42 +17,24 @@ document.getElementById("player_answer").innerHTML = "turno para responder - " +
 
 
 function send() {
-    get_word = document.getElementById("word").value;
-    word = get_word.toLowerCase();
-    console.log("Palabra en minúsculas = " + word);
-
-    charAt1 = word.charAt(1);
-    console.log(charAt1);
-
-    length_divide_2 = Math.floor(word.length/2);
-    charAt2 = word.charAt(length_divide_2);
-    console.log(charAt2);
-
-    length_minus_1 = word.length -1
-    charAt3 = word.charAt(length_minus_1);
-    console.log(charAt3);
-
-    remove_charAt1 = word.replace(charAt1, "_");
-    remove_charAt2 = remove_charAt1.replace(charAt2, "_");
-    remove_charAt3 = remove_charAt2.replace(charAt3, "_");
-    console.log(remove_charAt3);
-
-    question_word = "<h4 id='word_display'> Q.  "+remove_charAt3+"</h4>";
-    input_box = "<br>Respuesta : <input type='text' id='input_check_box'>";
-    check_button = "<br><br><button class= 'btn btn-info' onclick='check()'>comprobar</button>";
-    row = question_word + input_box + check_button ;
-    document.getElementById("output").innerHTML = row;
-    document.getElementById("word").value = "";
-}
-
+   number1 = document.getElementById("number1").value;
+   number2 = document.getElementById("number2").value;
+   actual_answer = parseInt(number1) * parseInt(number2);
+   
+   question_number = "<h4>" + number1 + " X "+ number2 +"</h4>";
+   input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+   check_button = "<br><br><button class='btn btn-info' onclick='check()'>comprobar</button>";
+   row = question_number + input_box + check_button ;
+   document.getElementById("output").innerHTML = row;
+   document.getElementById("number1").value = "";
+   document.getElementById("number2").value = "";
+ }
 question_turn = "player1";
 answer_turn = "player2";
 function check()
 {
     get_answer = document.getElementById("input_check_box").value;
-    answer = get_answer. toLowerCase();
-    console.log("Respuesta en minúsculas = " + answer);
-    if(answer == word)
+    if(get_answer == actual_answer)
     {
         if(answer_turn == "player1")
         {
@@ -65,24 +47,29 @@ function check()
             document.getElementById("player2_score").innerHTML = player2_score;  
         }
     }
-        if(question_turn == "player1")
+        
+    
+    
+    
+    if(question_turn == "player1")
         {
                 question_turn = "player2";
                 document.getElementById("player_question").innerHTML = "Turno para preguntar - " + player2_name ;
         }
-        else
+    else
         {
                 question_turn = "player1";
                 document.getElementById("player_question").innerHTML = "Turno para preguntar - " + player1_name ;
 
         }
-        if(answer_turn = "player1")
+        
+    if(answer_turn = "player1")
         {
                 answer_turn = "player2";
                 document.getElementById("player_answer").innerHTML = "Turno para responder - " + player2_name ;
 
         }
-        else
+    else
         {
                 answer_turn = "player1";
                 document.getElementById("player_answer").innerHTML = "Turno para responder - " + player1_name ; 
